fbd565d1a55b9aa6bd7413d51e28910621b11a2d
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-19 14:01:09 +0530

    Added security configuration classes

---
+++ src/main/java/dev/cse/imageannotatorbackend/config/AnnotatorSecurityConfig.java
@@ -0,0 +1,52 @@
+package dev.cse.imageannotatorbackend.config;
+
+import dev.cse.imageannotatorbackend.service.CustomAnnotatorDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class AnnotatorSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private CustomAnnotatorDetailsService annotatorDetailsService;
+	private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	private PasswordEncoder passwordEncoder; // Bean has been defined in UserSecurityConfig class
+
+	@Autowired
+	public AnnotatorSecurityConfig(CustomAnnotatorDetailsService annotatorDetailsService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint, PasswordEncoder passwordEncoder) {
+		this.annotatorDetailsService = annotatorDetailsService;
+		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(annotatorDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Handle logout on client side by changing basic authentication header username and password to "" and "" (empty or null)
+		http
+				.authorizeRequests()
+				.antMatchers("/", "/annotator/create").permitAll()
+				.antMatchers("/annotator/**").authenticated()
+				.anyRequest().authenticated()
+
+				.and()
+				.httpBasic()
+				.authenticationEntryPoint(customAuthenticationEntryPoint)
+
+				.and()
+				.csrf().disable();
+	}
+}
---
+++ src/main/java/dev/cse/imageannotatorbackend/config/UserSecurityConfig.java
@@ -0,0 +1,55 @@
+package dev.cse.imageannotatorbackend.config;
+
+import dev.cse.imageannotatorbackend.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@Order(2)
+public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private CustomUserDetailsService userDetailsService;
+	private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+	@Autowired
+	public UserSecurityConfig(CustomUserDetailsService userDetailsService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+		this.userDetailsService = userDetailsService;
+		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+	}
+
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Handle logout on client side by changing basic authentication header username and password to "" and "" (empty or null)
+		http
+				.authorizeRequests()
+				.antMatchers("/", "/user/create").permitAll()
+				.antMatchers("/user/**").authenticated()
+				.anyRequest().authenticated()
+
+				.and()
+				.httpBasic()
+				.authenticationEntryPoint(customAuthenticationEntryPoint)
+
+				.and()
+				.csrf().disable();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
