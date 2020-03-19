af68084a36266c32f048cec7525ab8cce7e997bf
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-19 14:02:17 +0530

    Added main Spring Application entry point

---
+++ src/main/java/dev/cse/imageannotatorbackend/ImageAnnotatorBackendApplication.java
@@ -0,0 +1,14 @@
+package dev.cse.imageannotatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAnnotatorBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ImageAnnotatorBackendApplication.class, args);
+	}
+
+}
