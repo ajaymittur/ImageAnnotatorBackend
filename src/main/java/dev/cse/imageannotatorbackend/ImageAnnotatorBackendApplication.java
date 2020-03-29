7f826ff86efabf397959dafa3c295acf5d2aff15
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-30 02:38:06 +0530

    Deleted extra empty line

--- src/main/java/dev/cse/imageannotatorbackend/ImageAnnotatorBackendApplication.java
+++ src/main/java/dev/cse/imageannotatorbackend/ImageAnnotatorBackendApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImageAnnotatorBackendApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ImageAnnotatorBackendApplication.class, args);
 	}
-
 }
