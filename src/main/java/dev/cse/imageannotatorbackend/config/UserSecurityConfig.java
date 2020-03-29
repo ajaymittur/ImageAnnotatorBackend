5144562fccf089211f480f61c987de5a460ef416
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-30 02:43:39 +0530

    Moved passwordEncoder bean and redid http filters

--- src/main/java/dev/cse/imageannotatorbackend/config/UserSecurityConfig.java
+++ src/main/java/dev/cse/imageannotatorbackend/config/UserSecurityConfig.java
@@ -1,15 +1,13 @@
 package dev.cse.imageannotatorbackend.config;
 
-import dev.cse.imageannotatorbackend.service.CustomUserDetailsService;
+import dev.cse.imageannotatorbackend.service.MySQLUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -17,39 +15,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Order(2)
 public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private CustomUserDetailsService userDetailsService;
+	private MySQLUserDetailsService userDetailsService;
 	private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	private PasswordEncoder passwordEncoder; // Bean has been defined in AnnotatorSecurityConfig class
 
 	@Autowired
-	public UserSecurityConfig(CustomUserDetailsService userDetailsService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+	public UserSecurityConfig(MySQLUserDetailsService userDetailsService, CustomAuthenticationEntryPoint customAuthenticationEntryPoint, PasswordEncoder passwordEncoder) {
 		this.userDetailsService = userDetailsService;
 		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Handle logout on client side by changing basic authentication header username and password to "" and "" (empty or null)
 		http
-				.authorizeRequests()
-				.antMatchers("/user/create").permitAll()
+				.csrf().disable()
+				.antMatcher("/user/**").authorizeRequests()
+				.antMatchers("/", "/user/create").permitAll()
 				.antMatchers("/user/**").hasRole("USER")
-				.anyRequest().permitAll()
 
 				.and()
 				.httpBasic()
 				.authenticationEntryPoint(customAuthenticationEntryPoint)
 
 				.and()
-				.csrf().disable();
+				.sessionManagement().disable();
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	// Configuration class to match all remaining requests
+//	@Configuration
+//	@Order(3)
+//	public static class RootSecurityConfig extends WebSecurityConfigurerAdapter {
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http
+//					.csrf().disable()
+//					.authorizeRequests().anyRequest().authenticated()
+//
+//					.and()
+//					.httpBasic()
+//
+//					.and()
+//					.sessionManagement().disable()
+//			;
+//		}
+//	}
 }
