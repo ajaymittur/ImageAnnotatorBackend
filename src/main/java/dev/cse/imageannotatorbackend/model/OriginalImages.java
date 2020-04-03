a29fa72dbb4c3bcdeb0d2b062f33dc2266cc9c66
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-04-03 22:50:02 +0530

    Added multivalued attributes

--- src/main/java/dev/cse/imageannotatorbackend/model/OriginalImages.java
+++ src/main/java/dev/cse/imageannotatorbackend/model/OriginalImages.java
@@ -1,6 +1,8 @@
 package dev.cse.imageannotatorbackend.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Original_Images")
@@ -10,23 +12,44 @@ public class OriginalImages {
 	@Id
 	@Column(name = "Image_Name")
 	private String name;
+
 	@Id
 	@Column(name = "Folder_Name")
 	private String folderName;
+
 	@Column(name = "Image_URL")
 	private String url;
+
 	@ManyToOne
 	@JoinColumn(name = "Username")
-	private Users user; // Enforces Username foreign key constraint
+	private Users user;
+
+	@ElementCollection
+	@CollectionTable(name = "Image_Categories", joinColumns = {
+			@JoinColumn(name = "Image_Name", referencedColumnName = "Image_Name"),
+			@JoinColumn(name = "Folder_Name", referencedColumnName = "Folder_Name")
+	})
+	@Column(name = "Category")
+	private Set<String> categories = new HashSet<>();
+
+	@ElementCollection
+	@CollectionTable(name = "Image_Tags", joinColumns = {
+			@JoinColumn(name = "Image_Name", referencedColumnName = "Image_Name"),
+			@JoinColumn(name = "Folder_Name", referencedColumnName = "Folder_Name")
+	})
+	@Column(name = "Tag")
+	private Set<String> tags = new HashSet<>();
 
 	public OriginalImages() {
 	}
 
-	public OriginalImages(Users user, String name, String folderName, String url) {
+	public OriginalImages(Users user, String name, String folderName, String url, Set<String> categories, Set<String> tags) {
 		this.user = user;
 		this.name = name;
 		this.folderName = folderName;
 		this.url = url;
+		this.categories = categories;
+		this.tags = tags;
 	}
 
 	public String getName() {
@@ -59,5 +82,21 @@ public class OriginalImages {
 
 	public void setUser(Users user) {
 		this.user = user;
+	}
+
+	public Set<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<String> categories) {
+		this.categories = categories;
+	}
+
+	public Set<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(Set<String> tags) {
+		this.tags = tags;
 	}
 }
