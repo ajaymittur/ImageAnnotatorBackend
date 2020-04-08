335e58e8b2142b3e60bab20fb8b55f98e9ca3608
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-04-08 23:55:46 +0530

    Added CORS config to allow all origins

--- src/main/java/dev/cse/imageannotatorbackend/ImageAnnotatorBackendApplication.java
+++ src/main/java/dev/cse/imageannotatorbackend/ImageAnnotatorBackendApplication.java
@@ -2,11 +2,24 @@ package dev.cse.imageannotatorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ImageAnnotatorBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageAnnotatorBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
