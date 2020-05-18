c5c948dd18abc4e8b9a534588071054c9569a5ec
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-05-19 01:18:18 +0530

    imported annotation

--- src/main/java/dev/cse/imageannotatorbackend/service/AnnotatorsService.java
+++ src/main/java/dev/cse/imageannotatorbackend/service/AnnotatorsService.java
@@ -4,6 +4,7 @@ import dev.cse.imageannotatorbackend.model.Annotators;
 import dev.cse.imageannotatorbackend.repository.AnnotatorsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
